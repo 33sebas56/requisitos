7733fa4e99928d014669643f40dfde8f9a2759af
33sebas56 <147175404+33sebas56@users.noreply.github.com>
2023-10-06 17:46:49 -0500

    Add files via upload

---
+++ #calculadora_1.py
@@ -0,0 +1,17 @@
+#calculadora
+#sebastian camilo ramos toro
+#06/10/2023
+print ("se realizaran cuatro operaciones basicas con los numeros que se ingresen")
+n_1 = float(input("por favor diguite un numero: "))
+n_2 = float(input("porfavor diguite su seguno numero: "))
+print ("los resultados de las operaciones son:")
+suma = n_1 + n_2
+print("la suma de ",n_1, " y ", n_2 , " es igual a: " ,suma)
+resta = n_1 - n_2
+print("la resta de ",n_1, " y ", n_2 , " es igual a: " ,resta)
+multiplica = n_1 * n_2
+print("la multiplicacion de ",n_1, " y ", n_2 , " es igual a: " ,multiplica)
+dividi = n_1 / n_2
+print("la division de ",n_1, " y ", n_2 , " es igual a: " ,dividi)
+
+
